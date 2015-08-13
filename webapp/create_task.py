32f4b23306ead6dace450d09ef670ecaa8da457d
Ubuntu <ubuntu@ip-10-0-1-192.ec2.internal>
2015-08-13 16:39:55 +0000

    added error handling

--- webapp/create_task.py
+++ webapp/create_task.py
@@ -1,6 +1,7 @@
 import boto3
 import time
 import pprint
+import socket
 
 def create_task(twitter_user):
     ecs = boto3.client('ecs')
@@ -32,26 +33,38 @@ def create_task(twitter_user):
     )
     
     task_arn = response['tasks'][0]['taskArn']
-    time.sleep(3)
-    desc = ecs.describe_tasks(tasks=[task_arn])
-    
-    networkBindings = desc['tasks'][0]['containers'][0]['networkBindings']
-    containerInstanceArn = desc['tasks'][0]['containerInstanceArn']
-    
-    containerDesc = ecs.describe_container_instances(containerInstances=[containerInstanceArn])
-    
-    ec2InstanceId = containerDesc['containerInstances'][0]['ec2InstanceId']
-    
-    ec2 = boto3.client('ec2')
-    ec2Instance = ec2.describe_instances(InstanceIds=[ec2InstanceId])
-    
-    #print "\n\n\n"
-    
-    ipAddress = ec2Instance['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    
-    #pp = pprint.PrettyPrinter(indent=4)
-    #pp.pprint(ec2Instance)
+
+    address_found = False
+
+    while not address_found:
+      time.sleep(2)
+
+      try: 
+        desc = ecs.describe_tasks(tasks=[task_arn])
+        networkBindings = desc['tasks'][0]['containers'][0]['networkBindings']
+        containerInstanceArn = desc['tasks'][0]['containerInstanceArn']
+
+        containerDesc = ecs.describe_container_instances(containerInstances=[containerInstanceArn])
+        ec2InstanceId = containerDesc['containerInstances'][0]['ec2InstanceId']
+
+        ec2 = boto3.client('ec2')
+        ec2Instance = ec2.describe_instances(InstanceIds=[ec2InstanceId])
+        ipAddress = ec2Instance['Reservations'][0]['Instances'][0]['PublicIpAddress']
+  
+        address_found = True
+      except Exception as e:
+        continue
     
     for index, binding in enumerate(networkBindings):
       if binding['containerPort'] == 7474:
+        neo4j_running = False
+        while not neo4j_running:
+          try:
+            s = socket.socket()
+            s.connect((ipAddress, binding['hostPort']))
+            neo4j_running = True
+          except Exception, e:
+            time.sleep(1)
+            continue
+
         return 'http://%s:%s' % (ipAddress, binding['hostPort'])
