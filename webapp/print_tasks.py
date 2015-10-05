c0aaca59c6cdfbdcfda74d5ad6e28ffb05e301ec
Ubuntu <ubuntu@ip-10-0-1-192.ec2.internal>
2015-10-05 01:49:04 +0000

    cleanup

--- webapp/print_tasks.py
+++ webapp/print_tasks.py
@@ -1,11 +1,5 @@
 import memcache
 import pprint
-from random_words import RandomWords
-
-#rw = RandomWords()
-#word = rw.random_words(count=3)
-
-#print '%s-%s-%s' % (word[0], word[1], word[2])
 
 pp = pprint.PrettyPrinter(indent=4)
 
