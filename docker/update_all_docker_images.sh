727adc980a19224c56db020130048ef229b32a28
ryguyrg <ryan@ryugy.com>
2015-10-07 15:33:59 -0700

    added bash script for docker image update on all neo4j machines.  requires ec2 client setup and ssh keys available

---
+++ docker/update_all_docker_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for ip in `ec2-describe-instances -F "tag:aws:autoscaling:groupName=ecs-neo4j-twitter" | grep NICASSOCIATION | cut -f 2`; do
+  ssh -i ~/keys/neo4j.pem ec2-user@$ip "docker pull ryguyrg/neo4j-twitter"
+done
