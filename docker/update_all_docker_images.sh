21f8d12cc42b2a61891630b63c6cb9d76ad517cb
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-27 12:03:11 +0530

    new Dockerfile

--- docker/update_all_docker_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for ip in `ec2-describe-instances -F "tag:aws:autoscaling:groupName=ecs-neo4j-twitter" | grep NICASSOCIATION | cut -f 2`; do
-  ssh -i ~/keys/devrel.pem ec2-user@$ip "docker pull ryguyrg/neo4j-twitter"
-done
