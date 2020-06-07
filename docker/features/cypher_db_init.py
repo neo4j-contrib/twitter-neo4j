4dc7806f5608910adf77478beb12c855552f7a70
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-06-07 15:25:25 +0530

    added docker compose for db init.Also added additional constraint

--- docker/features/cypher_db_init.py
+++ docker/features/cypher_db_init.py
@@ -15,6 +15,7 @@ constraint_list = [
     "CREATE CONSTRAINT ON (t:Tweet) ASSERT t.id IS UNIQUE;",
     "CREATE CONSTRAINT ON (u:User) ASSERT u.screen_name IS UNIQUE;",
     "CREATE CONSTRAINT ON (u:User) ASSERT u.id IS UNIQUE;",
+    "CREATE CONSTRAINT ON (u:User) ASSERT u.id_str IS UNIQUE;",
     "CREATE CONSTRAINT ON (h:Hashtag) ASSERT h.name IS UNIQUE;",
     "CREATE CONSTRAINT ON (l:Link) ASSERT l.url IS UNIQUE;",
     "CREATE CONSTRAINT ON (s:Source) ASSERT s.name IS UNIQUE;",
