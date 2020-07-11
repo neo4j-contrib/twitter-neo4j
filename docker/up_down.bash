6dee46fa02624ebd82e59be4fbf636049b0c7d5e
Deepak Kumar <33544658+krdpk17@users.noreply.github.com>
2020-07-11 21:01:13 +0530

    added command to display
    Not tested

--- docker/up_down.bash
+++ docker/up_down.bash
@@ -1,3 +1,4 @@
+set -x
 docker-compose -f docker-compose.yml -p production $*
 docker-compose -f docker-compose-dmbucket_monitor.yml -p production $*
 docker-compose -f docker-compose-dmcheck_batch1.yml -p production $*
