a62ebe4aef0f4e9f98230b819e085133404d8c5c
Deepak Kumar <33544658+krdpk17@users.noreply.github.com>
2020-07-11 21:02:58 +0530

    bugfix : removed the -p option
    not tested

--- docker/up_down.bash
+++ docker/up_down.bash
@@ -1,6 +1,6 @@
 set -x
-docker-compose -f docker-compose.yml -p production $*
-docker-compose -f docker-compose-dmbucket_monitor.yml -p production $*
-docker-compose -f docker-compose-dmcheck_batch1.yml -p production $*
-docker-compose -f docker-compose-dmcheck_batch2.yml -p production $*
+docker-compose -f docker-compose.yml $*
+docker-compose -f docker-compose-dmbucket_monitor.yml  $*
+docker-compose -f docker-compose-dmcheck_batch1.yml  $*
+docker-compose -f docker-compose-dmcheck_batch2.yml  $*
 
