37eb48d9ac5a093d642ba18fb744b56028295e5a
Deepak Kumar <33544658+krdpk17@users.noreply.github.com>
2020-07-26 14:04:15 +0530

    added missing checking

--- docker/features/libs/following_buckets_manager_client.py
+++ docker/features/libs/following_buckets_manager_client.py
@@ -1,5 +1,5 @@
 """
-This file is responsible to manage buckets
+This file is responsible to manage DM Check clients
 """
 
 '''
@@ -12,33 +12,23 @@ import time
 '''
 User defined modules
 '''
-from libs.twitter_logging import console_logger as logger
-from libs.cypher_store import FollowingCypherStoreIntf as StoreIntf
-from libs.cypher_store import ServiceManagementIntf as serviceIntf
+from libs.buckets_manager_client import BucketManagerClient
 
-from libs.client_manager import ClientManager
+from libs.cypher_store import FollowingCheckCypherStoreClientIntf as StoreIntf
+from libs.cypher_store import FollowingCheckCypherStoreCommonIntf as StoreCommonIntf
+from libs.cypher_store import ServiceManagementIntf as ServiceIDIntf
 
+class FollowingCheckBucketManagerClient(BucketManagerClient):
+    '''
+        It uses Facade design pattern
+    '''
 
-class utils:
-    @staticmethod
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]
-
-class FollowingsBucketManagerClient:
-
-    def __init__(self, client_id):
-        self.client_id = client_id
-        self.dataStoreIntf = StoreIntf()
-        self.client_manager = ClientManager()
-        self.service_manager = serviceIntf()
-        self.service_id = serviceIntf.ServiceIDs.FOLLOWING_SERVICE
-
-    def register_service_for_client(self):
-        client_id = self.client_id
+    def __init__(self, client_id, screen_name):
         #tested
-        print(("Registering service with ID {} for client {}".format(self.service_id, client_id)))
-        if not self.service_manager.client_service_registered(client_id=client_id, service_id=self.service_id):
-            self.service_manager.register_service_for_client(client_id=client_id, service_id=self.service_id)
-            print(("Successfully registered service with ID {} for client {}".format(self.service_id, client_id)))
+        self.dataStoreIntf = StoreIntf()
+        dataStoreCommonIntf = StoreCommonIntf()
+        service_id = ServiceIDIntf.ServiceIDs.FOLLOWING_SERVICE
+        super().__init__(client_id=client_id, screen_name=screen_name, service_id=service_id, dataStoreIntfObj=self.dataStoreIntf, dataStoreCommonIntfObj=dataStoreCommonIntf)
+    
+    def configure(self):
+        self.dataStoreIntf.configure(client_id=self.client_id)
