2da4d601b3c2880d0039c5d1c98c1fd77d240fde
Deepak Kumar <33544658+krdpk17@users.noreply.github.com>
2020-08-04 19:51:23 +0530

    bugfix: Fixed issue with non_dm info store
    Tested. Working

--- docker/features/libs/cypher_store_dmcheck.py
+++ docker/features/libs/cypher_store_dmcheck.py
@@ -34,14 +34,13 @@ class DMCheckCypherStoreClientIntf(ServiceCypherStoreClientIntf):
         print("Store DM data for {} bucket".format(bucket['bucket_id']))
         bucket_id = bucket['bucket_id']
         #TODO: Try to merge to single call
-        self.__store_dm_friends(client_id, bucket_id, bucket['candm_users'])
-        self.__store_nondm_friends(client_id, bucket_id, bucket['cantdm_users'])
-        self.__store_dmcheck_unknown_friends(client_id, bucket_id, bucket['unknown_users'])
+        self.__store_friends_dmstatus(client_id, bucket_id, bucket['candm_users'], "DM_YES")
+        self.__store_friends_dmstatus(client_id, bucket_id, bucket['cantdm_users'], "DM_NO")
+        self.__store_friends_dmstatus(client_id, bucket_id, bucket['unknown_users'], "DM_UNKNOWN")
         return
-
-    def __store_dm_friends(self, client_id, bucket_id, users):
-
-        print("Store DM users for {} bucket".format(bucket_id))
+    
+    def __store_friends_dmstatus(self, client_id, bucket_id, users, dm_link_name):
+        print("Store {} users for {} bucket".format(dm_link_name, bucket_id))
         state = {'client_id':client_id, 'bucket_id':bucket_id}
         query = """
             UNWIND $users AS user
@@ -51,44 +50,9 @@ class DMCheckCypherStoreClientIntf(ServiceCypherStoreClientIntf):
             MATCH(u:User {screen_name: user.screen_name})
             MATCH (u)-[r:INDMCHECKBUCKET]->()
             DELETE r
-            MERGE(u)<-[:DM_YES]-(client)
-        """
-        execute_query(query, users=users, state=state)
-        return True
-
-    def __store_nondm_friends(self, client_id, bucket_id, users):
-        
-        print("Store NON_DM users for {} bucket".format(bucket_id))
-        state = {'client_id':client_id, 'bucket_id':bucket_id}
-        query = """
-            UNWIND $users AS user
-
-            MATCH(client:DMCheckClient {id:$state.client_id})
-            MATCH(b:DMCheckBucket {uuid:$state.bucket_id})
-            MATCH(u:User {screen_name: user.screen_name})
-            MERGE(u)<-[:DM_NO]-(client)
-            WITH u
-            MATCH (u)-[r:INDMCHECKBUCKET]->()
-            DELETE r
-        """
-        execute_query(query, users=users, state=state)
-        return True
-
-    def __store_dmcheck_unknown_friends(self, client_id, bucket_id, users):
-        
-        print("Store Unknown users for {} bucket".format(bucket_id))
-        state = {'client_id':client_id, 'bucket_id':bucket_id}
-        query = """
-            UNWIND $users AS user
-
-            MATCH(client:DMCheckClient {id:$state.client_id})
-            MATCH(b:DMCheckBucket {uuid:$state.bucket_id})
-            MATCH(u:User {screen_name: user.screen_name})
             MERGE(u)<-[:DM_UNKNOWN]-(client)
-            WITH u
-            MATCH (u)-[r:INDMCHECKBUCKET]->()
-            DELETE r
         """
+        query = query.replace("DM_UNKNOWN", dm_link_name)
         execute_query(query, users=users, state=state)
         return True
 
