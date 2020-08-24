afa86f8752d56e8c1770cc0740786dfb28e3ffbc
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-08-24 08:47:08 +0530

    added all 5 batches of DMCheck
    Not tested

--- docker/up_down.bash
+++ docker/up_down.bash
@@ -3,4 +3,6 @@ docker-compose -f docker-compose.yml $*
 docker-compose -f docker-compose-dmbucket_monitor.yml  $*
 docker-compose -f docker-compose-dmcheck_batch1.yml  $*
 docker-compose -f docker-compose-dmcheck_batch2.yml  $*
-
+docker-compose -f docker-compose-dmcheck_batch3.yml  $*
+docker-compose -f docker-compose-dmcheck_batch4.yml  $*
+docker-compose -f docker-compose-dmcheck_batch5.yml  $*
